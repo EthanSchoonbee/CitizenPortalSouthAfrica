af97308558e67f805b073e44aee19d18d84f9cdb
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-09-11 20:09:03 +0200

    Inital Wizard & Form Resize

--- CitizenPortalSouthAfrica/Views/ReportIssuesView.xaml.cs
+++ CitizenPortalSouthAfrica/Views/ReportIssuesView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CitizenPortalSouthAfrica.ViewModels;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace CitizenPortalSouthAfrica.Views
@@ -12,6 +13,21 @@ namespace CitizenPortalSouthAfrica.Views
         {
             InitializeComponent();
             DataContext = new ReportIssuesViewModel();
+        }
+
+        private void LocationTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            GuideText.Text = "Please enter the location where the issue occurred.";
+        }
+
+        private void CategoryComboBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            GuideText.Text = "Select the category that best describes the issue.";
+        }
+
+        private void DescriptionRichTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            GuideText.Text = "Provide a detailed description of the issue.";
         }
     }
 }
