b3d61116497e3d0b43de8adaa65cd95e9061cd17
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-11-15 20:01:41 +0200

    Cleaned Up Report Status View

--- CitizenPortalSouthAfrica/Views/ReportStatusView.xaml.cs
+++ CitizenPortalSouthAfrica/Views/ReportStatusView.xaml.cs
@@ -7,10 +7,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -47,6 +49,46 @@ namespace CitizenPortalSouthAfrica.Views
                     viewModel.OnPropertyChanged(nameof(viewModel.Reports));
                 }
             }
+        }
+
+        private void OnToggleExpandCollapse(object sender, RoutedEventArgs e)
+        {
+            var toggleButton = sender as ToggleButton;
+            var report = toggleButton?.DataContext as Report; // Adjust based on your actual model
+
+            if (report != null)
+            {
+                var expandedContent = FindVisualChild<Grid>(toggleButton);
+                var animation = (Storyboard)FindResource("ExpandCollapseAnimation");
+
+                if (expandedContent.Visibility == Visibility.Collapsed)
+                {
+                    expandedContent.Visibility = Visibility.Visible;
+                    animation.Begin(expandedContent);
+                }
+                else
+                {
+                    expandedContent.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        // Helper method to find a child of type T
+        private static T FindVisualChild<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                if (child is T)
+                    return (T)child;
+                else
+                {
+                    T childOfChild = FindVisualChild<T>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
         }
     }
 }
