ac0053aa1137fb87d9fc5a6bb90a05cf5e1f2ac1
EthanSchoonbee <schoonbeeethan@gmail.com>
2024-11-15 15:24:53 +0200

    Added Navigation to Repoprt Status Page

---
+++ CitizenPortalSouthAfrica/ViewModels/ReportStatusViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CitizenPortalSouthAfrica.Models;
+using CitizenPortalSouthAfrica.Services;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CitizenPortalSouthAfrica.ViewModels
+{
+
+    public class ReportStatusViewModel: INotifyPropertyChanged
+    {
+        private readonly FileManagementService _fileManagementService; // Service for file operations
+        private readonly ValidationService _validationService; // Service for form validation
+        private readonly ReportIssueRepository _repository; // Repository for report issue data
+
+        // Commands for user actions
+        public ICommand NavigateToHomeCommand { get; } // Command to navigate to the home view
+        public ICommand NavigateToEventsAndAnnouncementsCommand { get; } // Command to navigate to the home view
+        public ICommand NavigateToReportIssuesCommand { get; } // Command to navigate to the request status view
+        public ICommand ExitCommand { get; } // Command to exit the application
+
+        // Constructor initializing services and commands
+        public ReportStatusViewModel()
+        {
+            // Initialize services and repository
+            _fileManagementService = new FileManagementService();
+            _validationService = new ValidationService();
+            _repository = new ReportIssueRepository();
+
+            // Initialize commands
+            ExitCommand = new RelayCommand(() => Services.NavigationService.GetInstance().ExitApplication()); // Command to exit the application
+            NavigateToHomeCommand = new RelayCommand(() => Services.NavigationService.GetInstance().NavigateTo(Constants.NavigationHeaders.Home));
+            NavigateToEventsAndAnnouncementsCommand = new RelayCommand(() => Services.NavigationService.GetInstance().NavigateTo(Constants.NavigationHeaders.EventsAndAnnouncements));
+            NavigateToReportIssuesCommand = new RelayCommand(() => Services.NavigationService.GetInstance().NavigateTo(Constants.NavigationHeaders.ReportIssues));
+        }
+
+        // Event to notify the UI of property changes
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Method to raise the PropertyChanged event
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
